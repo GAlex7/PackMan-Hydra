1a7ef5f56ba0b4b9ec8415aa634b53efd9d408a3
GAlex <GAlexGr@gmail.com>
2015-02-15 19:52:55 +0200

    proba

--- HydraPacMan/PackManHydra.cs
+++ HydraPacMan/PackManHydra.cs
@@ -4,63 +4,80 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-    class PackManHydra
+class PackManHydra
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int[,] walls = CreateWallArr();
+        // where the cells is 1 means that you can't go there
+
+        DrawGameBoard();
+
+        Console.WriteLine();
+        for (int r = 0; r < walls.GetLength(0); r++)
         {
-            int[,] walls = CreateWallArr();
-            // where the cells is 1 means that you can't go there
-
-            DrawGameBoard();
-
-            Console.WriteLine();
-            for (int r = 0; r < walls.GetLength(0); r++)
+            for (int c = 0; c < walls.GetLength(1); c++)
             {
-                for (int c = 0; c < walls.GetLength(1); c++)
-                {
-                    Console.Write(walls[r, c] == 1 ? 'M' : '.');
-                }
-                Console.WriteLine();
+                Console.Write(walls[r, c] == 1 ? 'M' : '.');
             }
-            Console.WriteLine("Our game board contains {0} rows and {1} cols.", walls.GetLength(0), walls.GetLength(1));
+            Console.WriteLine();
         }
-
-        private static void DrawGameBoard()
+        Console.WriteLine("Our game board contains {0} rows and {1} cols.", walls.GetLength(0), walls.GetLength(1));
+        ConsoleKeyInfo kk = Console.ReadKey(false);
+        if (kk.Key == ConsoleKey.LeftArrow)
         {
-            Console.WriteLine("┌────────────┐ ┌────────────┐");
-            Console.WriteLine("│............│ │............│");
-            Console.WriteLine("│.┌──┐.┌───┐.│ │.┌───┐.┌──┐.│");
-            Console.WriteLine("│.│  │.│ # │.│ │.│ # │.│  │.│");
-            Console.WriteLine("│@└──┘.└─┘└┘.└─┘.└┘└─┘.└──┘@│");
-            Console.WriteLine("│...........................│");
-            Console.WriteLine("│.┌──┐.┌┐.┌───────┐.┌┐.┌──┐.│");
-            Console.WriteLine("│.└──┘.││.└──┐ ┌──┘.││.└──┘.│");
-            Console.WriteLine("│......││....│ │....││......│");
-            Console.WriteLine("└────┐.│└──┐ │ │ ┌──┘│.┌────┘");
-            Console.WriteLine("     │.│┌──┘ └─┘ └──┐│.│     ");
-            Console.WriteLine("     │.││           ││.│     ");
-            Console.WriteLine("─────┘.└┘ ┌─┐┌─┐┌─┐ └┘.└─────");
-            Console.WriteLine("      .   │ #   # │   .      ");
-            Console.WriteLine("─────┐.┌┐ └───────┘ ┌┐.┌─────");
-            Console.WriteLine("     │.││   READY!  ││.│     ");
-            Console.WriteLine("     │.││ ┌───────┐ ││.│     ");
-            Console.WriteLine("┌────┘.└┘ └──┐ ┌──┘ └┘.└────┐");
-            Console.WriteLine("│............│ │............│");
-            Console.WriteLine("│.┌──┐.┌───┐.│ │.┌───┐.┌──┐.│");
-            Console.WriteLine("│.└─┐│.└───┘.└─┘.└───┘.│┌─┘.│");
-            Console.WriteLine("│@..││....... X .......││..@│");
-            Console.WriteLine("└─┐.││.┌┐.┌───────┐.┌┐.││.┌─┘");
-            Console.WriteLine("┌─┘.└┘.││.└──┐ ┌──┘.││.└┘.└─┐");
-            Console.WriteLine("│......││....│ │....││......│");
-            Console.WriteLine("│.┌────┘└──┐.│ │.┌──┘└────┐.│");
-            Console.WriteLine("│.└────────┘.└─┘.└────────┘.│");
-            Console.WriteLine("│...........................│");
-            Console.WriteLine("└───────────────────────────┘");
+            Console.WriteLine("You pressed Left Arrow");
         }
-
-        private static int[,] CreateWallArr()
+        if (kk.Key == ConsoleKey.RightArrow)
         {
-            int[,] wallsArr = new int[,]
+            Console.WriteLine("You pressed Right Arrow");
+        }
+        if (kk.Key == ConsoleKey.UpArrow)
+        {
+            Console.WriteLine("You pressed Up Arrow");
+        }
+        if (kk.Key == ConsoleKey.DownArrow)
+        {
+            Console.WriteLine("You pressed Down Arrow");
+        }
+    }
+
+    private static void DrawGameBoard()
+    {
+        Console.WriteLine("┌────────────┐ ┌────────────┐");
+        Console.WriteLine("│............│ │............│");
+        Console.WriteLine("│.┌──┐.┌───┐.│ │.┌───┐.┌──┐.│");
+        Console.WriteLine("│.│  │.│ # │.│ │.│ # │.│  │.│");
+        Console.WriteLine("│@└──┘.└─┘└┘.└─┘.└┘└─┘.└──┘@│");
+        Console.WriteLine("│...........................│");
+        Console.WriteLine("│.┌──┐.┌┐.┌───────┐.┌┐.┌──┐.│");
+        Console.WriteLine("│.└──┘.││.└──┐ ┌──┘.││.└──┘.│");
+        Console.WriteLine("│......││....│ │....││......│");
+        Console.WriteLine("└────┐.│└──┐ │ │ ┌──┘│.┌────┘");
+        Console.WriteLine("     │.│┌──┘ └─┘ └──┐│.│     ");
+        Console.WriteLine("     │.││           ││.│     ");
+        Console.WriteLine("─────┘.└┘ ┌─┐┌─┐┌─┐ └┘.└─────");
+        Console.WriteLine("      .   │ #   # │   .      ");
+        Console.WriteLine("─────┐.┌┐ └───────┘ ┌┐.┌─────");
+        Console.WriteLine("     │.││   READY!  ││.│     ");
+        Console.WriteLine("     │.││ ┌───────┐ ││.│     ");
+        Console.WriteLine("┌────┘.└┘ └──┐ ┌──┘ └┘.└────┐");
+        Console.WriteLine("│............│ │............│");
+        Console.WriteLine("│.┌──┐.┌───┐.│ │.┌───┐.┌──┐.│");
+        Console.WriteLine("│.└─┐│.└───┘.└─┘.└───┘.│┌─┘.│");
+        Console.WriteLine("│@..││....... X .......││..@│");
+        Console.WriteLine("└─┐.││.┌┐.┌───────┐.┌┐.││.┌─┘");
+        Console.WriteLine("┌─┘.└┘.││.└──┐ ┌──┘.││.└┘.└─┐");
+        Console.WriteLine("│......││....│ │....││......│");
+        Console.WriteLine("│.┌────┘└──┐.│ │.┌──┘└────┐.│");
+        Console.WriteLine("│.└────────┘.└─┘.└────────┘.│");
+        Console.WriteLine("│...........................│");
+        Console.WriteLine("└───────────────────────────┘");
+    }
+
+    private static int[,] CreateWallArr()
+    {
+        int[,] wallsArr = new int[,]
             {
             {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
             {1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
@@ -92,7 +109,27 @@ using System.Text;
             {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
             {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
             };
-            return wallsArr;
-        }
+        return wallsArr;
     }
+
+    //if (Console.KeyAvailable)
+    //            {
+    //                ConsoleKeyInfo userInput = Console.ReadKey();
+    //                if (userInput.Key == ConsoleKey.LeftArrow)
+    //                {
+    //                    if (direction != right) direction = left;
+    //                }
+    //                if (userInput.Key == ConsoleKey.RightArrow)
+    //                {
+    //                    if (direction != left) direction = right;
+    //                }
+    //                if (userInput.Key == ConsoleKey.UpArrow)
+    //                {
+    //                    if (direction != down) direction = up;
+    //                }
+    //                if (userInput.Key == ConsoleKey.DownArrow)
+    //                {
+    //                    if (direction != up) direction = down;
+    //                }
+    //            }
 }
