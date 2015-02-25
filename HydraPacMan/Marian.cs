9cdbceb7d1b3267176bcc19cb4a1696c2ed4f00a
mdraganov <mariyandraganov@gmail.com>
2015-02-25 20:45:54 +0200

    kdfjjsdghjksd

--- HydraPacMan/Marian.cs
+++ HydraPacMan/Marian.cs
@@ -7,5 +7,23 @@ using System.Threading.Tasks;
 class Marian
 {
     // Гад 4 - > W
+
+    public static void MonsterW()
+    {
+        PackManHydra.badGuysCoordinates[4, 0] = PackManHydra.badGuysCoordinates[4, 2];
+        PackManHydra.badGuysCoordinates[4, 1] = PackManHydra.badGuysCoordinates[4, 3];
+        PackManHydra.badGuysCoordinates[4, 2] = PackManHydra.monsterW[PackManHydra.GadFourCounter, 0];
+        PackManHydra.badGuysCoordinates[4, 3] = PackManHydra.monsterW[PackManHydra.GadFourCounter, 1];
+        if (PackManHydra.GadFourCounter == 101)
+        {
+            PackManHydra.GadFourCounter = 0;
+        }
+        else
+        {
+            PackManHydra.GadFourCounter++;
+        }
+        PackManHydra.badGuysCoordinates[4, 2] = PackManHydra.monsterW[PackManHydra.GadFourCounter, 0];
+        PackManHydra.badGuysCoordinates[4, 3] = PackManHydra.monsterW[PackManHydra.GadFourCounter, 1];
+    }
 }
 
