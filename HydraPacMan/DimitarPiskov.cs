4a32c0cf638b8bfeb2b6357a3563c7e1bda5f078
Dimitar Piskov <littleimp@abv.bg>
2015-02-27 18:34:05 +0200

    new change

--- HydraPacMan/DimitarPiskov.cs
+++ HydraPacMan/DimitarPiskov.cs
@@ -29,44 +29,44 @@ class DimitarPiskov
         for (int i = 0; i < 31; i++)
         {
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.SetCursorPosition(i, 8);
+            Console.SetCursorPosition(i, 1);
             Console.Write("*");
-            Console.SetCursorPosition(i, 24);
+            Console.SetCursorPosition(i, 32);
             Console.Write("*");
         }
         Console.ForegroundColor = ConsoleColor.White;
-        Console.SetCursorPosition(7, 9);
+        Console.SetCursorPosition(7, 1);
         Console.Write("Congratulations!");
-        Console.SetCursorPosition(1, 10);
+        Console.SetCursorPosition(1, 3);
         Console.Write(" You just entered the castle");
-        Console.SetCursorPosition(1, 11);
+        Console.SetCursorPosition(1, 5);
         Console.Write("   of Telerik. As a future  ");
-        Console.SetCursorPosition(1, 12);
+        Console.SetCursorPosition(1, 7);
         Console.Write(" .NET Ninja you must collect");
-        Console.SetCursorPosition(1, 13);
+        Console.SetCursorPosition(1, 9);
         Console.Write("     all \"#\" symbols.     ");
-        Console.SetCursorPosition(1, 14);
+        Console.SetCursorPosition(1, 11);
         Console.Write("The dark forces of the castle");
-        Console.SetCursorPosition(1, 15);
+        Console.SetCursorPosition(1, 13);
         Console.Write("  (the lectors - Д, Е, И, Н)");
-        Console.SetCursorPosition(1, 16);
+        Console.SetCursorPosition(1, 15);
         Console.Write("will do anything to stop you.");
         Console.SetCursorPosition(1, 17);
         Console.Write("Once they catch you - you're");
-        Console.SetCursorPosition(1, 18);
-        Console.Write("dead so try to stay away from");
         Console.SetCursorPosition(1, 19);
-        Console.Write("them! After you collect(eat)");
-        Console.SetCursorPosition(1, 20);
-        Console.Write("all the \"#\" symbols you go");
+        Console.Write("dead so try to stay away from");
         Console.SetCursorPosition(1, 21);
-        Console.Write(" to the next level. Pass all");
-        Console.SetCursorPosition(1, 22);
-        Console.Write("the levels and become a .NET");
+        Console.Write("them! After you collect(eat)");
         Console.SetCursorPosition(1, 23);
+        Console.Write("all the \"#\" symbols you go");
+        Console.SetCursorPosition(1, 25);
+        Console.Write(" to the next level. Pass all");
+        Console.SetCursorPosition(1, 27);
+        Console.Write("the levels and become a .NET");
+        Console.SetCursorPosition(1, 29);
         Console.Write("      Ninja! Good luck!     ");
         Console.CursorVisible = false;
-        for (int i = 9; i < 24; i++)
+        for (int i = 1; i < 31; i++)
         {
             Console.ForegroundColor = ConsoleColor.Green;
             Console.SetCursorPosition(0, i);
